d3ab5bd657a0440735f3e09899818207d167b79e
Juan <juanmahc0109@gmail.com>
2023-05-08 09:13:51 -0500

    Correccion de ruta de imagenes

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -11,7 +11,7 @@ function Movie( {movieData} ) {
         <div className='card'>
             {
                 movieData.imageUrl === null ? (
-                    <img src='../../../public/no_image.png' alt="No image available" />
+                    <img src='../../public/no_image.png' alt="No image available" />
                 ) : (
                     <img src={`${movieImage}${movieData.imageUrl}`} alt={movieData.name} />
                 )
