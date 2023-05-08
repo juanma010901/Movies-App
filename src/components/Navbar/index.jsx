e749d7eef2c56cb64f3a67e2ae9ccdef7885859c
Juan <juanmahc0109@gmail.com>
2023-05-08 09:31:31 -0500

    Correccion de ruta de imagenes

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -16,7 +16,7 @@ function Navbar() {
     return (
         <>
             <nav className='navbar'>
-                <a href="/Movies-App"><img src="../../logo.svg" alt="Movie-App" id="icon"/></a>
+                <a href="/Movies-App"><img src="../../assets/logo.svg" alt="Movie-App" id="icon"/></a>
                 <h2 id="title">Movie App</h2>
                 <div className="buscar">
                     <button className="boton-nav" id="cancelar" type="submit">
